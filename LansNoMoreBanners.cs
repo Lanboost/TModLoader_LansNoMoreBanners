f9b664f6a9ef645c5efeb8afe54bb491e5dc6e58
Lanboost <lanboost@hotmail.com>
2019-10-26 20:32:26 +0200

    Second

--- LansNoMoreBanners.cs
+++ LansNoMoreBanners.cs
@@ -1,7 +1,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace NoMoreBanners
+namespace LansNoMoreBanners
 {
     public class NoMoreBanners : Mod
     {
--- NoMoreBannersGlobalItem.cs
+++ NoMoreBannersGlobalItem.cs
@@ -7,7 +7,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace NoMoreBanners
+namespace LansNoMoreBanners
 {
     class NoMoreBannersGlobalItem : GlobalItem
     {
